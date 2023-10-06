f41108e6d07d8756ec2b535f3bca3538dc4e6f29
Zakaria6789sn <93602335+Zakaria6789sn@users.noreply.github.com>
2023-10-06 18:30:11 +0200

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderPrincipaleComponent } from './home/header-principale/header-principale.component';
+import { AboutComponent } from './home/about/about.component';
+import { EsperienzaComponent } from './home/esperienza/esperienza.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeaderPrincipaleComponent,
+    AboutComponent,
+    EsperienzaComponent,
   ],
   imports: [
     BrowserModule,
